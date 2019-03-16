71f60d35edaf65f5e296c133d79a941a16dfafcb
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-03-16 08:53:03 -0500

    yarn it

--- js/index.ts
+++ js/index.ts
@@ -9,3 +9,8 @@ window.addEventListener('DOMContentLoaded', () => {
   db.doIt()
   console.log('Hello World (from index)', environment)
 })
+
+// TODO: Use this for client ID
+const arr = new Uint32Array(2)
+crypto.getRandomValues(arr)
+console.log('64-bit Random ID:', `${arr[0]}${arr[1]}`)
