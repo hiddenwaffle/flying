3f0f9db923635f9cb3424b3c4f261e2c52b7b1de
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-03-17 12:50:51 -0500

    Add timer copy paste

--- js/index.ts
+++ js/index.ts
@@ -11,6 +11,5 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 // TODO: Use this for client ID
-const arr = new Uint32Array(2)
-crypto.getRandomValues(arr)
-console.log('64-bit Random ID:', `${arr[0]}-${arr[1]}`)
+const id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+console.log('Client ID:', id)
