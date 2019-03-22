6898105e58f95f62b75a008b3d374b3b3f274504
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-03-22 00:28:25 -0500

    Fix texture size of ground to be power of 2

--- js/gfx/gfx.ts
+++ js/gfx/gfx.ts
@@ -28,6 +28,14 @@ export class Gfx {
     this.ui.start()
 
     const scene = new BABYLON.Scene(this.engine)
+    // const camera = new BABYLON.ArcRotateCamera(
+    //   'camera',
+    //   0, // -Math.PI / 2, // lon (e-w)
+    //   Math.PI / 2, // Math.PI / 4, // lat (n-s)
+    //   5,
+    //   new BABYLON.Vector3(0, 1, 0), // BABYLON.Vector3.Zero(),
+    //   scene
+    // )
     const camera = new BABYLON.ArcRotateCamera(
       'camera',
       0, // -Math.PI / 2, // lon (e-w)
@@ -37,13 +45,36 @@ export class Gfx {
       scene
     )
     camera.attachControl(this.canvasTmp)
+    const sphere0 = BABYLON.MeshBuilder.CreateSphere('sphere0', { }, scene)
+    camera.lockedTarget = sphere0
+
+    BABYLON.AbstractMesh.prototype.spin = function (axis: any, rads: any, speed: any) {
+        var ease = new BABYLON.CubicEase();
+        ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+      BABYLON.Animation.CreateAndStartAnimation('at4', this, 'rotation.' + axis, speed, 120, this.rotation[axis], this.rotation[axis]+rads, 0, ease);
+    }
+    BABYLON.ArcRotateCamera.prototype.spin = function (property: any, rads: any, speed: any) {
+        var ease = new BABYLON.CubicEase();
+        ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+      BABYLON.Animation.CreateAndStartAnimation('at4', this, property, speed, 120, this[property], this[property]+rads, 0, ease);
+    }
+    BABYLON.AbstractMesh.prototype.spinTo = function (unused: any, targetRot: any, speed: any) {
+        var ease = new BABYLON.CubicEase();
+        ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+      BABYLON.Animation.CreateAndStartAnimation('at4', this, 'rotation', speed, 120, this.rotation, targetRot, 0, ease);
+    }
+    setTimeout(() => {
+      sphere0.spin('y', Math.PI / 4, 50)
+      camera.spin('alpha', -Math.PI / 4, 50)
+    }, 1000)
+
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(-1, 1, 0), scene)
 
     const ground = BABYLON.MeshBuilder.CreateGround('ground', { height: 1000, width: 1000 }, scene)
     const groundMaterial = new BABYLON.StandardMaterial('groundMaterial', scene)
     groundMaterial.diffuseTexture = new BABYLON.Texture(fieldsImage, scene)
-    groundMaterial.diffuseTexture.uScale = 20
-    groundMaterial.diffuseTexture.vScale = 20
+    groundMaterial.diffuseTexture.uScale = 25
+    groundMaterial.diffuseTexture.vScale = 25
     ground.material = groundMaterial
 
     const skybox = BABYLON.MeshBuilder.CreateBox('skyBox', { size: 1000 }, scene)
@@ -64,8 +95,6 @@ export class Gfx {
     skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0)
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
     skybox.material = skyboxMaterial
-
-    const sphere0 = BABYLON.MeshBuilder.CreateSphere('sphere0', { }, scene)
 
     const building0 = BABYLON.MeshBuilder.CreateBox(
       'building0',
