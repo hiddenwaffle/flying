775b1d7628bff3df94260263458bd4f7e5d3dc5e
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-03-27 23:44:15 -0500

    Have test 8 origin y+ following correctly

--- js/babylon-playground-testing-8.js
+++ js/babylon-playground-testing-8.js
@@ -1,7 +1,6 @@
-// This is a continuation of testing #2
-// Well documented but doesn't quite work right because the poles
-// warp the path, the great circle isn't followed, and the direction flips
-// after crossing certain thresholds
+// When moving forward, parent to origin
+// When turning, be parent to origin
+
 
 // Helper function from:
 // https://www.babylonjs-playground.com/#MYY6S#7
@@ -10,17 +9,55 @@ function asCartesianToRef(rho, phi, theta, ref) {
   const x = rho * Math.cos(theta) * Math.sin(phi)
   const y = rho * Math.cos(phi)
   const z = rho * Math.sin(theta) * Math.sin(phi)
-
   ref.set(x, y, z)
 }
 
-// // Helper function from:
-// // http://www.html5gamedevs.com/topic/7599-convert-global-coordinates-to-local-coordinates/
-// function globalToLocal(vector, mesh) {
-//     var m = new BABYLON.Matrix();
-//     mesh.getWorldMatrix().invertToRef(m);
-//     return BABYLON.Vector3.TransformCoordinates(vector, m);
-// }
+// Helper function from:
+// http://www.html5gamedevs.com/topic/21494-keep-childs-world-position-when-parenting/
+// https://www.babylonjs-playground.com/#28IXSE#13
+function parentMesh(child, parent) {
+  var rotation = BABYLON.Quaternion.Identity();
+  var position = BABYLON.Vector3.Zero();
+  var m1 = BABYLON.Matrix.Identity();
+  var m2 = BABYLON.Matrix.Identity();
+
+  parent.getWorldMatrix().decompose(BABYLON.Vector3.Zero(), rotation, position);
+  rotation.toRotationMatrix(m1);
+  m2.setTranslation(position);
+  m2.multiplyToRef(m1, m1);
+  var invParentMatrix = BABYLON.Matrix.Invert(m1);
+  var m = child.getWorldMatrix().multiply(invParentMatrix);
+  m.decompose(BABYLON.Vector3.Zero(), child.rotationQuaternion, position);
+  invParentMatrix = BABYLON.Matrix.Invert(parent.getWorldMatrix());
+  var m = child.getWorldMatrix().multiply(invParentMatrix);
+  m.decompose(BABYLON.Vector3.Zero(), BABYLON.Quaternion.Identity(), position);
+
+  child.position.x = position.x * parent.scaling.x;
+  child.position.y = position.y * parent.scaling.y;
+  child.position.z = position.z * parent.scaling.z;
+
+  if (parent.scaling.x != 1 || parent.scaling.y != 1 || parent.scaling.z != 1) {
+  var children = parent.getChildren();
+  var scaleFixMesh;
+  for (var i = 0; i < children.length; i++) {
+      if (children[i].name == 'scaleFixMesh') {
+      scaleFixMesh = children[i];
+      //alert("found");
+      break;
+      }
+  }
+  if (scaleFixMesh == undefined) {
+      scaleFixMesh = new BABYLON.Mesh('scaleFixMesh', parent.getScene());
+      scaleFixMesh.parent = parent;
+  }
+  scaleFixMesh.scaling.x = 1 / parent.scaling.x;
+  scaleFixMesh.scaling.y = 1 / parent.scaling.y;
+
+  child.parent = scaleFixMesh;
+  } else {
+      child.parent = parent;
+  }
+}
 
 var createScene = function () {
   console.clear()
@@ -44,14 +81,16 @@ var createScene = function () {
   //  (   "Left Side", Equator) = (       PI / 2 , Math.PI * 1/2 )
   //  (Prime Meridian, Equator) = (       PI     , Math.PI * 1/2 ) <-- facing camera
   //  (  "Right Side", Equator) = (      3PI / 2 , Math.PI * 1/2 )
-  var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(-3, 0, 0), scene);
+  // TEMPORARY: Look at origin from an angle
+  var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(-2, 1.4, 1.4), scene);
+  // var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(-3, 0, 0), scene);
   camera.speed = 0.25
   camera.setTarget(BABYLON.Vector3.Zero());
   camera.attachControl(canvas, true);
   var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
   light.intensity = 0.7;
 
-  // Globe sphere
+  // Params: name, subdivs, size, scene
   var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', { segments: 16 }, scene);
   sphere.scaling = new BABYLON.Vector3(2, 2, 2)
   sphere.bakeCurrentTransformIntoVertices()
@@ -96,23 +135,47 @@ var createScene = function () {
   var dtheta = -Math.sin(myAngle) // Should match code in loop
   asCartesianToRef(rho, phi, theta, ship.position)
 
-  // Origin sphere at 0, 0, 0
-  var origin = BABYLON.MeshBuilder.CreateSphere('origin', { segments: 16 }, scene);
+  // Origin box at 0, 0, 0
+  var origin = BABYLON.MeshBuilder.CreateBox('origin', { }, scene);
   origin.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05)
   origin.bakeCurrentTransformIntoVertices()
   origin.rotationQuaternion = new BABYLON.Quaternion()
-  ship.parent = origin
 
+  // "movement" rotation for origin box
+  oAxis = new BABYLON.Vector3(1, 0, 0)
+  oAngle = 0
+
+  // Draw visualization: line from origin to ship
+  var originToShipRay = new BABYLON.Ray(BABYLON.Vector3.Zero(), BABYLON.Vector3.Zero(), 1); // Values do not seem to matter when attached to mesh?
+  var originToShipRayHelper = new BABYLON.RayHelper(originToShipRay)
+  originToShipRayHelper.show(scene)
+  // Draw visualization of oAxis - initially in the direction of the +x axis
+  var oAxisRay = new BABYLON.Ray(BABYLON.Vector3.Zero(), BABYLON.Vector3.Zero(), 1); // Values do not seem to matter when attached to mesh?
+  var oAxisRayHelper = new BABYLON.RayHelper(oAxisRay)
+  oAxisRayHelper.show(scene, new BABYLON.Color3(0, 0.5, 1)) // skyblue
+  // Draw negative direction
+  var oAxisRayN = new BABYLON.Ray(new BABYLON.Vector3(1, 0, 0), BABYLON.Vector3.Zero(), 1); // Values do not seem to matter when attached to mesh?
+  var oAxisRayNHelper = new BABYLON.RayHelper(oAxisRayN)
+  oAxisRayNHelper.show(scene, new BABYLON.Color3(1, 0.5, 0)) // orange
+
+  // Old cache variables TODO: convert these
   const scratch = new BABYLON.Quaternion()
   const scratch2 = new BABYLON.Quaternion()
   const scratchVector = new BABYLON.Vector3()
   const myAxis = new BABYLON.Vector3()
 
+  // Cache variables for beforeRender callback, use with caution
+  const q1cache = new BABYLON.Quaternion()
+  const q2cache = new BABYLON.Quaternion()
+  const v1cache = new BABYLON.Vector3()
+  const v2cache = new BABYLON.Vector3()
+  const v3cache = new BABYLON.Vector3()
+  const m1cache = new BABYLON.Matrix()
+  const zaxis = new BABYLON.Vector3(0, 0, 1)
+
   function debug() {
-      // asCartesianToRef(rho, theta, phi, scratchVector)
-      // console.log('(', phi, ',', theta, ') vs:', scratchVector)
-      console.log(origin.rotationQuaternion)
-      // TODO: Try parenting only when moving forward? But what about rotation?
+      asCartesianToRef(rho, theta, phi, scratchVector)
+      console.log('(', phi, ',', theta, ') vs:', scratchVector)
   }
 
   scene.beforeRender = () => {
@@ -149,37 +212,42 @@ var createScene = function () {
           //                 looking  "down" =>   sine => +1   to theta
           dphi   = -Math.cos(myAngle)
           dtheta = -Math.sin(myAngle)
-          // console.log('-->', dphi, dtheta, myAngle)
 
-          // TODO: Reconstruct the great circle
+          // Point origin +y at ship
+          ship.getDirectionToRef(BABYLON.Axis.Z, v1cache)
+          v2cache.copyFrom(ship.position)
+          BABYLON.Vector3.CrossToRef(v1cache, v2cache, v3cache)
+          BABYLON.Quaternion.RotationQuaternionFromAxisToRef(v1cache, v2cache, v3cache, origin.rotationQuaternion)
       }
       if (map[' ']) {
           // Instead of doing the next three lines, must follow the great circle
           phi +=   dphi   * 0.05
           theta += dtheta * 0.05
           asCartesianToRef(rho, theta, phi, ship.position)
-
-          // TODO: Somehow this needs to correct towards the great circle if it is getting off track
-          // TODO: Somehow this also needs to handle direction correctly when wrapping around
-
           debug()
       }
 
-      // if (map[' ']) {
-      //     // Calculate moving in the current direction one frame (TODO: Use time)
-      //     ship.translate(BABYLON.Axis.Z, 0.05, BABYLON.Space.LOCAL);
-      //     // TODO: Instead of directly doing above, move the guide along the geodesic
-      //     //       Then translate the ship to the guide
-      //     //       Do not recompute the geodesic unless turning with A or D keys
-
-      //     // "drop" the ship towards the origin so it is the expected distance away.
-      //     // Good thing 1 is the distance used here, can just normalize...
-      //     ship.position.normalize()
-      //     // TODO: Possibility of rotating WITHOUT parenting by computing sin/cos of theta/phi changes
-      //     // TODO: Quaternion slerping?
-      //     // TODO: Maybe it is possible to get the axis of rotation, keep it constant, and
-      //     //       update it only when turning? Angle mvmt/frame is fixed rather than accumulating
-      // }
+      // Visualize the line from origin to ship
+      originToShipRayHelper.attachToMesh(
+          origin,
+          new BABYLON.Vector3(0, 1, 0), // direction in local mesh
+          new BABYLON.Vector3(0, 0, 0), // origin in local mesh
+          1 // length
+      )
+      // Visualize the oAxis
+      oAxisRayHelper.attachToMesh(
+          origin,
+          oAxis,                        // direction in local mesh
+          new BABYLON.Vector3(0, 0, 0), // origin in local mesh
+          2 // length
+      )
+      oAxis.scaleToRef(-1, v1cache)
+      oAxisRayNHelper.attachToMesh(
+          origin,
+          v1cache,                      // direction in local mesh
+          new BABYLON.Vector3(0, 0, 0), // origin in local mesh
+          2 // length
+      )
   }
 
   return scene;
