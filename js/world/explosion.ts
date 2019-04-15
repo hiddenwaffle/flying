ac9d9312654e1308dc30bd0dfd8ad3cbc1e66793
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2019-04-14 21:48:43 -0500

    Add explosion

--- js/world/explosion.ts
+++ js/world/explosion.ts
@@ -3,6 +3,8 @@ import { Projectile } from './projectile'
 // TODO: Easing function that is fast and then suddenly slower
 
 export class Explosion extends Projectile {
+  private ttl = 0
+
   constructor(
     id: number,
     private readonly meshSphere: any,
@@ -11,22 +13,47 @@ export class Explosion extends Projectile {
   ) {
     super(id, scene.getAnimationRatio.bind(scene))
     this.meshSphere.parent = this.arrow
+    this.meshSphere.position.y = -1
+    this.meshSphere.position.z = -1
     this.minSpeed = this.currentSpeed = 0
     this.setEnabled(false)
   }
 
   step() {
     super.step()
-    // TODO: Advance the explosion animation
+    this.ttl -= 16.66 * this.getAnimationRatio() // Assumes 60 fps
+    if (this.ttl <= 0) {
+      this.setEnabled(false)
+      this.returnToPool(this.id)
+    } else {
+      this.increaseSize()
+    }
   }
 
   explode(x: number, y: number, z: number, w: number) {
-    console.log('BOOM ', this.id, x, y, z, w, this.meshSphere)
+    this.ttl = 125
+    this.setSize(0.1)
     this.setQ(x, y, z, w)
     this.setEnabled(true)
   }
 
   private setEnabled(enabled: boolean) {
     this.meshSphere.setEnabled(enabled)
+  }
+
+  private resetSize() {
+    this.setSize(0.1)
+  }
+
+  private increaseSize() {
+    this.meshSphere.scaling.x += 1 * this.getAnimationRatio()
+    this.meshSphere.scaling.y += 1 * this.getAnimationRatio()
+    this.meshSphere.scaling.z += 1 * this.getAnimationRatio()
+  }
+
+  private setSize(size: number) {
+    this.meshSphere.scaling.x = size
+    this.meshSphere.scaling.y = size
+    this.meshSphere.scaling.z = size
   }
 }
